2e94aecadfd89bdb6b985456099bfe553a35b826
CleberSouza <souzacleber.cs.cs@gmail.com>
2025-04-07 13:34:59 -0300

    first commit

---
+++ assets/js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+  
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  });
+  
